1c1107abbff92db30652fe074a1d8b129beb4960
4geru <uchinishi.koichi@moneyforward.co.jp>
2020-05-12 22:08:37 +0900

    udpate linebots_controller

---
+++ app/controllers/linebots_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class LinebotsController < ApplicationController
+  # LINE APIを返すときはつける必要がある
+  protect_from_forgery :except => [:create]
+  before_action :validate_signature, only: [:create]
+
+  def create
+    client.parse_events_from(body).each do |event|
+      client.reply_message(event['replyToken'], message(event))
+    end
+    head :ok
+  end
+
+  private
+
+  def body
+    @body ||= request.body.read
+  end
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    end
+  end
+
+  def validate_signature
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    head :bad_request unless client.validate_signature(body, signature)
+  end
+
+  def message(event)
+    # ここに書いていく
+    case event
+    when Line::Bot::Event::Message
+      {
+        type: 'text',
+        text: event['message']['text']
+      }
+    end
+  end
+end
